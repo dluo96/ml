e463007c8dcaecf25a3fc00dd207bfd208a5815d
Daniel Luo <dan.luo1996@gmail.com>
2024-08-27 17:56:47 +0100

    chore: rename

--- lm/bigram/bigram_test.py
+++ lm/bigram/bigram_test.py
@@ -24,16 +24,16 @@ class TestBigram(unittest.TestCase):
         self.assertEqual(self.model.make_bigrams(["emma"]), expected_bigrams)
 
     def test_count_bigrams(self):
-        expected_bg_counts = {
+        expected_bigram_counts = {
             ("<S>", "e"): 1,
             ("e", "m"): 1,
             ("m", "m"): 1,
             ("m", "a"): 1,
             ("a", "<E>"): 1,
         }
-        self.assertEqual(self.model.count_bigrams(["emma"]), expected_bg_counts)
+        self.assertEqual(self.model.count_bigrams(["emma"]), expected_bigram_counts)
 
-    def test_create_bigram_tensor(self):
+    def test_create_bigram_tensor__one_word_dataset(self):
         # Overwrite model instance in `setUp` to test with a 1-word dataset
         self.model = Bigram(words=["emma"])
         expected_tensor_emma = torch.tensor(
