92b43f5be71a9df750d6f6c91b83deddfa938b64
Daniel Luo <dan.luo1996@gmail.com>
2024-08-27 17:52:41 +0100

    refactor: move bigram logic into class

--- lm/bigram/bigram.py
+++ lm/bigram/bigram.py
@@ -1,117 +1,107 @@
 import torch
 
 
-def make_bigrams(words: list[str]) -> list[tuple[str, str]]:
-    bigrams = []
-    for w in words:
-        chs = ["<S>"] + list(w) + ["<E>"]
-        for ch1, ch2 in zip(chs, chs[1:]):  # zip truncates the longer iterable
-            bigrams.append((ch1, ch2))
-    return bigrams
+class Bigram:
+    def __init__(self, words: list[str]) -> None:
+        self.words = words
+        unique_chars = ["."] + sorted(list(set("".join(words))))
+        self.ctoi = {c: i for i, c in enumerate(unique_chars)}
+        self.itoc = {i: c for i, c in enumerate(unique_chars)}
+        self.n_unique_chars = len(unique_chars)
 
+    def create_bigram_tensor(self, words: list[str]) -> torch.Tensor:
+        # Use a single token "." in favour of "<S>" and "<E>"
+        bigram_tensor = torch.zeros(
+            (self.n_unique_chars, self.n_unique_chars),
+            dtype=torch.int32,
+        )
+        for w in words:
+            chs = ["."] + list(w) + ["."]
+            for ch1, ch2 in zip(chs, chs[1:]):
+                ix1 = self.ctoi[ch1]
+                ix2 = self.ctoi[ch2]
+                bigram_tensor[ix1, ix2] += 1
 
-def count_bigrams(words: list[str]) -> dict[tuple[str, str], int]:
-    bg_counts = {}
-    for w in words:
-        chs = ["<S>"] + list(w) + ["<E>"]
-        for ch1, ch2 in zip(chs, chs[1:]):
-            bigram = (ch1, ch2)
-            bg_counts[bigram] = bg_counts.get(bigram, 0) + 1
-    return bg_counts
+        return bigram_tensor
 
+    def make_bigrams(self, words: list[str]) -> list[tuple[str, str]]:
+        bigrams = []
+        for w in words:
+            chs = ["<S>"] + list(w) + ["<E>"]
+            for ch1, ch2 in zip(chs, chs[1:]):  # zip truncates the longer iterable
+                bigrams.append((ch1, ch2))
+        return bigrams
 
-def create_bigram_tensor(words: list[str]) -> torch.Tensor:
-    # Use a single token "." in favour of "<S>" and "<E>"
-    unique_chars = ["."] + sorted(list(set("".join(words))))
-    N = len(unique_chars)
-    bigram_tensor = torch.zeros((N, N), dtype=torch.int32)
-    ctoi = {c: i for i, c in enumerate(unique_chars)}
-    for w in words:
-        chs = ["."] + list(w) + ["."]
-        for ch1, ch2 in zip(chs, chs[1:]):
-            ix1 = ctoi[ch1]
-            ix2 = ctoi[ch2]
-            bigram_tensor[ix1, ix2] += 1
+    def count_bigrams(self, words: list[str]) -> dict[tuple[str, str], int]:
+        bg_counts = {}
+        for w in words:
+            chs = ["<S>"] + list(w) + ["<E>"]
+            for ch1, ch2 in zip(chs, chs[1:]):
+                bigram = (ch1, ch2)
+                bg_counts[bigram] = bg_counts.get(bigram, 0) + 1
+        return bg_counts
 
-    return bigram_tensor
+    def sample(self, words: list[str]) -> str:
+        bigram_tensor = self.create_bigram_tensor(words)
 
+        # Compute matrix of probabilities
+        bigram_probs = bigram_tensor.float()
+        bigram_probs /= bigram_probs.sum(dim=1, keepdim=True)
 
-def sample(words: list[str]) -> str:
-    unique_chars = ["."] + sorted(list(set("".join(words))))
-    itoc = {i: c for i, c in enumerate(unique_chars)}
-    bigram_tensor = create_bigram_tensor(words)
+        g = torch.Generator().manual_seed(2147483647)
+        out = []
+        ix = 0  # Start token "." is first
+        while True:
+            # Get the probability distribution (over all possible characters)
+            # for the next character given the current character
+            p = bigram_probs[ix]
 
-    # Compute matrix of probabilities
-    bigram_probs = bigram_tensor.float()
-    bigram_probs /= bigram_probs.sum(dim=1, keepdim=True)
+            # Sample a character index based on the bigram distribution
+            ix = torch.multinomial(
+                p, num_samples=1, replacement=True, generator=g
+            ).item()
 
-    g = torch.Generator().manual_seed(2147483647)
-    out = []
-    ix = 0  # Start token "." is first
-    while True:
-        # Get the probability distribution (over all possible characters)
-        # for the next character given the current character
-        p = bigram_probs[ix]
+            # Append character
+            out.append(self.itoc[ix])
 
-        # Sample a character index based on the bigram distribution
-        ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
+            # Stop if we sample the end token "<E>"
+            if ix == 0:
+                break
 
-        # Append character
-        out.append(itoc[ix])
+        return "".join(out)
 
-        # Stop if we sample the end token "<E>"
-        if ix == 0:
-            break
+    def evaluate(self, words: list[str]) -> float:
+        """Evaluate the model by computing the average negative log likelihood (NLL).
 
-    return "".join(out)
+        Goal is to maximise the likelihood of the data with respect to the model
+        parameters, which, for the case of the bigram model, are the elements of the
+        bigram probability matrix.
 
+        This is equivalent to maximising the log likelihood (because log is monotonic).
+        This is equivalent to minimising the negative log likelihood.
+        This is equivalent to minimising the average negative log likelihood.
+        """
+        # Create matrix of bigram probabilities
+        # Smoothing is applied to avoid -inf in the negative log likelihood which would
+        # happen for bigrams that never occurred in the dataset. The larger the smoothing
+        # parameter, the closer we get to a uniform distribution
+        smoothing_param = 0
+        bigram_tensor = self.create_bigram_tensor(words)
+        bigram_probs = (bigram_tensor + smoothing_param).float()
+        bigram_probs /= bigram_probs.sum(dim=1, keepdim=True)
 
-def evaluate(words: list[str]) -> float:
-    """Evaluate the model by computing the average negative log likelihood (NLL).
+        log_likelihood = 0.0
+        n = 0
+        for w in words:
+            chs = ["."] + list(w) + ["."]
+            for ch1, ch2 in zip(chs, chs[1:]):
+                ix1 = self.ctoi[ch1]
+                ix2 = self.ctoi[ch2]
+                prob = bigram_probs[ix1, ix2]
+                log_prob = torch.log(prob)
+                log_likelihood += log_prob.item()
+                n += 1
 
-    Goal is to maximise the likelihood of the data with respect to the model
-    parameters, which, for the case of the bigram model, are the elements of the
-    bigram probability matrix.
-
-    This is equivalent to maximising the log likelihood (because log is monotonic).
-    This is equivalent to minimising the negative log likelihood.
-    This is equivalent to minimising the average negative log likelihood.
-    """
-    unique_chars = ["."] + sorted(list(set("".join(words))))
-    ctoi = {c: i for i, c in enumerate(unique_chars)}
-
-    # Create matrix of bigram probabilities
-    # Smoothing is applied to avoid -inf in the negative log likelihood which would
-    # happen for bigrams that never occurred in the dataset. The larger the smoothing
-    # parameter, the closer we get to a uniform distribution
-    smoothing_param = 0
-    bigram_tensor = create_bigram_tensor(words)
-    bigram_probs = (bigram_tensor + smoothing_param).float()
-    bigram_probs /= bigram_probs.sum(dim=1, keepdim=True)
-
-    log_likelihood = 0.0
-    n = 0
-    for w in words[:3]:
-        chs = ["."] + list(w) + ["."]
-        for ch1, ch2 in zip(chs, chs[1:]):
-            ix1 = ctoi[ch1]
-            ix2 = ctoi[ch2]
-            prob = bigram_probs[ix1, ix2]
-            log_prob = torch.log(prob)
-            log_likelihood += log_prob.item()
-            n += 1
-            print(f"{ch1}{ch2}: {prob:.4f} {log_prob:.4f}")
-
-    nll = -log_likelihood
-
-    print(f"{log_likelihood=}")
-    print(f"{nll=}")
-    print(f"{nll/n=}")
-
-    avg_nll = nll / n
-    return avg_nll
-
-
-if __name__ == "__main__":
-    names = open("../names.txt", "r").read().splitlines()
-    evaluate(names)
+        nll = -log_likelihood
+        avg_nll = nll / n
+        return avg_nll
--- lm/bigram/bigram_test.py
+++ lm/bigram/bigram_test.py
@@ -1,65 +1,71 @@
+import pathlib
+import unittest
+
 import torch
 
-from lm.bigram import (
-    count_bigrams,
-    create_bigram_tensor,
-    evaluate,
-    make_bigrams,
-    sample,
-)
+from lm.bigram.bigram import Bigram
 
 
-def test_make_bigrams():
-    assert make_bigrams(["emma"]) == [
-        ("<S>", "e"),
-        ("e", "m"),
-        ("m", "m"),
-        ("m", "a"),
-        ("a", "<E>"),
-    ]
+class TestBigram(unittest.TestCase):
+    def setUp(self):
+        # Setup that runs before each test
+        data_dir = pathlib.Path(__file__).parent.parent
+        self.words = open(f"{data_dir}/names.txt", "r").read().splitlines()
+        self.model = Bigram(words=self.words)
 
+    def test_make_bigrams(self):
+        expected_bigrams = [
+            ("<S>", "e"),
+            ("e", "m"),
+            ("m", "m"),
+            ("m", "a"),
+            ("a", "<E>"),
+        ]
+        self.assertEqual(self.model.make_bigrams(["emma"]), expected_bigrams)
 
-def test_count_bigrams():
-    assert count_bigrams(["emma"]) == {
-        ("<S>", "e"): 1,
-        ("e", "m"): 1,
-        ("m", "m"): 1,
-        ("m", "a"): 1,
-        ("a", "<E>"): 1,
-    }
+    def test_count_bigrams(self):
+        expected_bg_counts = {
+            ("<S>", "e"): 1,
+            ("e", "m"): 1,
+            ("m", "m"): 1,
+            ("m", "a"): 1,
+            ("a", "<E>"): 1,
+        }
+        self.assertEqual(self.model.count_bigrams(["emma"]), expected_bg_counts)
 
-
-def test_create_bigram_tensor():
-    assert torch.equal(
-        create_bigram_tensor(["emma"]),
-        torch.tensor(
+    def test_create_bigram_tensor(self):
+        # Overwrite model instance in `setUp` to test with a 1-word dataset
+        self.model = Bigram(words=["emma"])
+        expected_tensor_emma = torch.tensor(
             [
                 [0, 0, 1, 0],  # (".", "e")
                 [1, 0, 0, 0],  # ("a", ".")
                 [0, 0, 0, 1],  # ("e", "m")
                 [0, 1, 0, 1],  # ("m", "a") and ("m", "m")
             ]
-        ),
-    )
-    assert torch.equal(
-        create_bigram_tensor(["ava"]),
-        torch.tensor(
+        )
+        actual_tensor_emma = self.model.create_bigram_tensor(["emma"])
+        self.assertTrue(torch.equal(expected_tensor_emma, actual_tensor_emma))
+
+        # Reset model instance to test with a different 1-word dataset
+        self.model = Bigram(words=["ava"])
+        expected_tensor_ava = torch.tensor(
             [
                 [0, 1, 0],  # (".", "a")
                 [1, 0, 1],  # ("a", "v") and ("a", ".")
                 [0, 1, 0],  # ("v", "a")
             ]
-        ),
-    )
+        )
+        actual_tensor_ava = self.model.create_bigram_tensor(["ava"])
+        self.assertTrue(torch.equal(expected_tensor_ava, actual_tensor_ava))
+
+    def test_sample(self):
+        self.assertEqual(self.model.sample(self.words), "cexze.")
+
+    def test_evaluate(self):
+        avg_nll = self.model.evaluate(self.words)
+        self.assertAlmostEqual(avg_nll, 2.454, places=4)
 
 
-def test_sample():
-    words = open("../names.txt", "r").read().splitlines()
-    sampled_name = sample(words)
-    assert sampled_name == "cexze."
-
-
-def test_evaluate():
-    words = open("../names.txt", "r").read().splitlines()
-    avg_nll = evaluate(words)
-    assert round(avg_nll, 4) == 2.4241
+if __name__ == "__main__":
+    unittest.main()
