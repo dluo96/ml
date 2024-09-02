2c1901ff1a33de2ba7b99f5c1b1089a33066f46c
Daniel Luo <dan.luo1996@gmail.com>
2024-09-02 11:03:51 +0100

    feat(sequence_dataset): add get_output_length method needed to extract block size for RNN training

--- lm/datasets/sequence_dataset.py
+++ lm/datasets/sequence_dataset.py
@@ -16,6 +16,9 @@ class SequenceDataset(Dataset):
     def get_vocab_size(self) -> int:
         return len(self.unique_chars)
 
+    def get_output_length(self) -> int:
+        return self.max_word_length
+
     def encode(self, word: str) -> torch.Tensor:
         ix = torch.tensor([self.ctoi[ch] for ch in word])
         return ix
