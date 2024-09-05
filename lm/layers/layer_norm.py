7f3416cc0b7e8f44258ce6b4e1d77dc2ac47f6d2
Daniel Luo <dan.luo1996@gmail.com>
2024-09-05 15:32:23 +0100

    feat(layer_norm): implementation of LayerNorm as well as an associated test class

---
+++ lm/layers/layer_norm.py
@@ -0,0 +1,31 @@
+import torch
+
+
+class LayerNorm:
+    """The key difference with BatchNorm is that LayerNorm computes mean and variance
+    across the feature dimension (dim=1) instead of the batch dimension (dim=0).
+    Because the calculation of mean and variance is not across examples, we don't need
+    a running mean and variance. It also follows that there is no distinction between
+    training and evaluation.
+    """
+
+    def __init__(self, n_embd: int, eps: float = 1e-5):
+        self.eps = eps
+
+        # Parameters (trained with backpropagation)
+        self.gamma = torch.ones(n_embd)  # Determines scale
+        self.beta = torch.zeros(n_embd)  # Determines shift
+
+    def parameters(self) -> list[torch.Tensor]:
+        return [self.gamma, self.beta]
+
+    def __call__(self, x: torch.Tensor) -> torch.Tensor:
+        # Key difference with BatchNorm is that LayerNorm computes mean and variance
+        # across the feature dimension (dim=1) instead of the batch dimension (dim=0)
+        xmean = x.mean(dim=1, keepdim=True)
+        xvar = x.var(dim=1, keepdim=True, unbiased=True)
+
+        xhat = (x - xmean) / torch.sqrt(xvar + self.eps)
+        self.out = self.gamma * xhat + self.beta
+
+        return self.out
---
+++ lm/layers/layer_norm_test.py
@@ -0,0 +1,56 @@
+import unittest
+
+import torch
+
+from lm.layers.layer_norm import LayerNorm
+
+
+class TestLayerNorm(unittest.TestCase):
+    def setUp(self):
+        self.n_embd = 5
+        self.batch_size = 32
+        self.eps = 1e-5
+        self.bn = LayerNorm(n_embd=self.n_embd, eps=self.eps)
+
+    def test_init(self):
+        self.assertEqual(self.bn.gamma.shape, (self.n_embd,))
+        self.assertEqual(self.bn.beta.shape, (self.n_embd,))
+        self.assertTrue(torch.equal(self.bn.gamma, torch.ones(self.n_embd)))
+        self.assertTrue(torch.equal(self.bn.beta, torch.zeros(self.n_embd)))
+
+    def test_parameters(self):
+        # Ensure that the parameters method returns gamma and beta
+        params = self.bn.parameters()
+        self.assertEqual(
+            len(params), 2, "There should be two parameters (gamma and beta)!"
+        )
+        self.assertIs(params[0], self.bn.gamma, "First parameter should be gamma!")
+        self.assertIs(params[1], self.bn.beta, "Second parameter should be beta!")
+
+    def test_forward(self):
+        x = torch.randn(self.batch_size, self.n_embd)
+        out = self.bn(x)
+
+        # Check the output shape
+        self.assertEqual(
+            out.shape,
+            (self.batch_size, self.n_embd),
+            msg="Output shape must be (batch_size, n_embd)",
+        )
+
+        # Check that the mean and variance, calculated across the embedding dimension,
+        # are (close to) 0 and 1, respectively
+        expected_mean = torch.zeros(self.n_embd, dtype=torch.float32)
+        self.assertTrue(
+            torch.allclose(out.mean(dim=1), expected_mean, atol=1e-3),
+            msg="Mean of the LayerNorm output should be (close to) 0",
+        )
+        expected_var = torch.ones(self.n_embd, dtype=torch.float32)
+        self.assertTrue(
+            torch.allclose(out.var(dim=1), expected_var, atol=1e-3),
+            msg="Variance of the LayerNorm output should be (close to) 1",
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
