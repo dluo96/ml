f3915e05a65239cbfdbafc97f472723b95f60963
Daniel Luo <dan.luo1996@gmail.com>
2024-09-16 13:59:07 +0100

    feat(regex_tokenizer): initial implementation and test

---
+++ lm/tokenization/regex_tokenizer.py
@@ -0,0 +1,44 @@
+import regex as re
+
+from lm.tokenization.tokenizer import BytePairEncodingTokenizer
+
+GPT4_SPLIT_PATTERN = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""  # fmt: skip
+
+
+class RegexTokenizer(BytePairEncodingTokenizer):
+    def __init__(self):
+        super().__init__()
+        self.compiled_pattern = re.compile(GPT4_SPLIT_PATTERN)
+
+    def train(self, text: str, final_vocab_size: int) -> None:
+        num_merges = final_vocab_size - 256
+
+        subtexts = re.findall(self.compiled_pattern, text)
+
+        # Convert text to raw bytes
+        tokens = [list(subtext.encode("utf-8")) for subtext in subtexts]
+
+        merges = {}
+        vocab = {idx: bytes([idx]) for idx in range(256)}
+        for i in range(num_merges):
+            pair_counts = {}
+            for subtext in subtexts:
+                # Passing in `pair_counts` will update it in place, adding up counts
+                pair_counts = self._get_pair_counts(subtext)
+
+            # Get the most frequent pair
+            top_pair = max(pair_counts, key=pair_counts.get)
+
+            # Index for the new token
+            idx = 256 + i
+
+            # Merge: replace all occurrences of `top_pair` in `tokens` with idx
+            print(f"Merging {top_pair=} into a new token with {idx=}")
+            tokens = [self._merge(t, top_pair, idx) for t in tokens]
+
+            # Update dictionaries
+            merges[top_pair] = idx
+            vocab[idx] = vocab[top_pair[0]] + vocab[top_pair[1]]
+
+        self.merges = merges
+        self.vocab = vocab
---
+++ lm/tokenization/regex_tokenizer_test.py
@@ -0,0 +1,91 @@
+import unittest
+
+import regex as re
+
+
+def test_regex():
+    # Define the regex pattern. Note that | means "or"
+    pattern = re.compile(r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+""")  # fmt: skip
+
+    # 's, 't, 're, 've, 'm, 'll, 'd correspond to common contractions
+    assert re.findall(pattern, "Hello how're you") == ["Hello", " how", "'re", " you"]
+
+    # ` ?\p{L}+` represents an optional space followed by one or more letters
+    assert re.findall(pattern, "Hello how are you") == ["Hello", " how", " are", " you"]
+
+    # ` ?\p{N}+` represents an optional space followed by one or more numbers
+    # NOTE: letters and numbers are separated!
+    assert re.findall(pattern, "Hello how123 are you") == [
+        "Hello",
+        " how",
+        "123",
+        " are",
+        " you",
+    ]
+
+    # ` ?[^\s\p{L}\p{N}]+` represents an optional space followed by one or more
+    # characters that are not letters or numbers. This capture punctuation.
+    assert re.findall(pattern, "Hello how are you!!!?") == [
+        "Hello",
+        " how",
+        " are",
+        " you",
+        "!!!?",
+    ]
+
+    # `\s+(?!\S)` represents one or more spaces that are not followed by a non-space character
+    assert re.findall(pattern, "Hello how are         you?") == [
+        "Hello",
+        " how",
+        " are",
+        "        ",
+        " you",
+        "?",
+    ]
+
+    # Should have added re.IGNORECASE to `pattern` so BPE merges can happen for
+    # capitalized versions of contractions. We can see the wrong behaviour this leads
+    # to in this example:
+    assert re.findall(pattern, "Hello HOW'RE you") == [
+        "Hello",
+        " HOW",
+        "'",
+        "RE",
+        " you",
+    ]
+
+    # `\s+` will catch any trailing spaces
+    assert re.findall(pattern, "Hello how are you    ") == [
+        "Hello",
+        " how",
+        " are",
+        " you",
+        "    ",
+    ]
+
+    """Instead of encoding the text directly, we first split it up.
+    So,
+        1. First, the text is split into a list of subtexts.
+        2. Each subtext is processed independently by the tokenizer.
+        3. The results of the separate processes are concatenated.
+        4. We only ever find merges between the characters within a single subtext.
+            For example, the space in " you" would never be merged with the "e" in
+            " are" because they belong to separate subtexts.
+        5. Once we have done the merges for each subtext, the results are combined.
+        6. This approach prevents certain undesirable merges.
+
+    NOTE: the GPT-2 tokenizer does not merge multiple whitespaces. This is unlike the
+    GPT-4 tokenizer.
+    """
+
+    """GPT-2 has 50257 tokens in its vocabulary. This consists of
+        - 256 raw byte tokens
+        - 50,000 additional tokens from using the BPE algorithm (50,000 merges)
+        - 1 special token '<|endoftext|>', used to delimit documents in the training
+            dataset. This signals to the LLM that it has reached the end of a document.
+            We expect the language model to learn the meaning of this token.
+    """
+
+
+if __name__ == "__main__":
+    unittest.main()
