4b2902a3da2cfa020b7f5759a36d5ea5fbe7e4b5
Daniel Luo <dan.luo1996@gmail.com>
2024-09-17 09:37:24 +0100

    refactor: move gpt regex split patterns into their module and created an additional test file

--- lm/tokenization/regex_tokenizer.py
+++ lm/tokenization/regex_tokenizer.py
@@ -1,8 +1,7 @@
 import regex as re
 
+from lm.tokenization.split_patterns import GPT4_SPLIT_PATTERN
 from lm.tokenization.utils import get_pair_counts, merge_new_token
-
-GPT4_SPLIT_PATTERN = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""  # fmt: skip
 
 
 class RegexTokenizer:
---
+++ lm/tokenization/split_patterns.py
@@ -0,0 +1,2 @@
+GPT2_SPLIT_PATTERN = r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""  # fmt: skip
+GPT4_SPLIT_PATTERN = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""  # fmt: skip
