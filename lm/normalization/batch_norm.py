96d2655bfa09be9155519a755ef72133542fbf43
Daniel Luo <dan.luo1996@gmail.com>
2024-09-19 14:48:51 +0100

    docs(batch_norm): improve the docstring

--- lm/normalization/batch_norm.py
+++ lm/normalization/batch_norm.py
@@ -3,10 +3,10 @@ import torch
 
 class BatchNorm1D:
     """Batch normalization is used to control the statistics of activations in a
-    neural network. It helps ensure that the pre-activation values are not too
-    spread out or too close together. This can help with training stability because it
-    can prevent the gradients from becoming too small or too large. It is common to put
-    it after layers with a multiplication, such as a linear layer or a convolutional
+    neural network. It helps ensure that the pre-activation values are not too spread
+    out or too close together. This can help with training stability because it can
+    prevent the gradients from becoming too small or too large. It is common to put it
+    after layers with a multiplication, such as a linear layer or a convolutional
     layer.
 
     Consider the case where the activation function is tanh(x). If x is too spread out,
@@ -17,6 +17,16 @@ class BatchNorm1D:
     Before batch normalization, ML practitioners avoided the saturation problem by
     initialising the neural network weights carefully such as using Kaiming/He or
     Xavier initialisation.
+
+    The batch norm layer computes the mean and variance of the pre-activations feeding
+    into it, across the batch. It has trainable (via backpropagation) parameters gamma
+    and beta for scaling and shifting, respectively. This allows the distribution of
+    the pre-activations (output of batch norm layer) to deviate from the standard
+    normal distribution (mean 0 and variance 1) if necessary.
+
+    The batch norm layer also keeps a running mean and a running variance, which are
+    used during evaluation/inference. In particular, it allows us to forward individual
+    examples at inference time.
     """
 
     def __init__(self, n_embd: int, eps: float = 1e-5, momentum: float = 0.1):
