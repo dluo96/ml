25cad6a16c7f1b6995a7251a63488463b26f1eaa
Daniel Luo <dan.luo1996@gmail.com>
2024-10-29 10:29:04 +0000

    docs(adam): drastically better comment and docstring

--- lm/optimizers/adam.py
+++ lm/optimizers/adam.py
@@ -4,9 +4,9 @@ from lm.tensor import Tensor
 class AdamOptimizer:
     """Minimal implementation of Adam (Adaptive Moment Estimation).
 
-    Adam combines momentum and RMSProp. As such, it relies on two moments:
-        - First-order moment estimate of the mean,
-        - Second-order moment estimate of the variance.
+    Adam combines (gradient descent with) Momentum and (gradient descent with) RMSProp:
+        - Momentum: uses an exponentially weighted moving average of gradients.
+        - RMSProp: uses an exponentially weighted moving average of squared gradients.
 
     NOTE: this implementation has neither weight decay nor learning rate decay.
     It simply updates weights and biases based on Adam's momentum and RMSProp steps
@@ -33,51 +33,49 @@ class AdamOptimizer:
         self.epsilon = epsilon
         self.lr = lr
 
-        # Initialize the (exponentially weighted) moving average of the gradient `dw`.
-        # This serve as a first moment estimate.
+        # Momentum: initialize the (exponentially weighted) moving average of the
+        # gradient `dw`.
         self.m_dw = 0
 
-        # Initialize the (exponentially weighted) moving average of the squared
-        # gradient `dw**2`. This serves as second moment estimate.
+        # RMSProp: initialize the (exponentially weighted) moving average of the
+        # squared gradient `dw**2`.
         self.v_dw = 0
 
     def update(self, t: int, w: Tensor, dw: Tensor) -> Tensor:
         """Update the weights and biases using the Adam optimizer.
 
-        First, do the momentum-like update (exponentially weighted average):
-
+        - Momentum update:
             m_t = beta_1 * m_{t-1} + (1 - beta_1) * g_t
 
-        Then, do the RMSProp-like update:
-
+        - RMSProp update:
             v_t = beta_2 * v_{t-1} + (1 - beta_2) * g_t^2
 
-        Since most algorithms that depend on moving averages (such as SGD and RMSProp)
-        are biased, we need a bias correction step:
+        - Bias correction:
+            m_t_corr = m_t / (1 - beta_1^t)
+            v_t_corr = v_t / (1 - beta_2^t)
 
-            m_t_hat = m_t / (1 - beta_1^t)
-            v_t_hat = v_t / (1 - beta_2^t)
+            This improves the accuracy of initial exponentially weighted averages
+            which are typically very small if the moving averages are initialized to 0.
 
-        Finally, we update the weights and biases:
-
-            w_t = w_{t-1} - lr / (sqrt(v_t_hat) + epsilon) * m_t_hat
+        - Parameter update:
+            w_t = w_{t-1} - lr / (sqrt(v_t_corr) + epsilon) * m_t_corr
 
         Args:
             t: current iteration.
             w: weights.
-            dw: gradients of `w` in the current batch.
+            dw: gradients of `w` (wrt. the loss function) in the current batch.
         """
-        # Update moving average of gradient
+        # Momentum: update moving average of gradients
         self.m_dw = self.beta_1 * self.m_dw + (1 - self.beta_1) * dw
 
-        # Update moving average of squared gradient
+        # RMSProp: update moving average of squared gradients
         self.v_dw = self.beta_2 * self.v_dw + (1 - self.beta_2) * dw**2
 
-        # Bias correction
-        m_dw_hat = self.m_dw / (1 - self.beta_1**t)
-        v_dw_hat = self.v_dw / (1 - self.beta_2**t)
+        # Bias corrections
+        m_dw_corr = self.m_dw / (1 - self.beta_1**t)
+        v_dw_corr = self.v_dw / (1 - self.beta_2**t)
 
-        # Update weights and biases
-        w = w - self.lr * m_dw_hat / (v_dw_hat.sqrt() + self.epsilon)
+        # Parameter update
+        w = w - self.lr * m_dw_corr / (v_dw_corr.sqrt() + self.epsilon)
 
         return w
