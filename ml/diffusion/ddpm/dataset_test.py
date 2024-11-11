6baf6a6158c9a71fbb295a2e82107bd3fd5184a0
Daniel Luo <dan.luo1996@gmail.com>
2024-11-11 17:20:29 +0000

    test: dataset for ddpm

--- ml/diffusion/ddpm/dataset_test.py
+++ ml/diffusion/ddpm/dataset_test.py
@@ -1,12 +1,30 @@
 import unittest
 
-from torch.utils.data.dataset import ConcatDataset, Dataset
+import torch
+from torch.utils.data.dataset import Dataset
 
-from ml.diffusion.ddpm.dataset import create_datasets
+from ml.diffusion.ddpm.dataset import IMG_SIZE, create_datasets
 
 
 class TestDataset(unittest.TestCase):
+    """This will run slowly if the torchvision datasets have not been downloaded
+    locally yet.
+    """
+
     def test_create_datasets(self):
         train_dataset, test_dataset = create_datasets()
         assert isinstance(train_dataset, Dataset)
         assert isinstance(test_dataset, Dataset)
+
+        # Do a few checks on an example data point
+        x, y = train_dataset[0]
+
+        # Check image shape
+        assert x.shape == (3, IMG_SIZE, IMG_SIZE)
+
+        # Check label is integer
+        assert isinstance(y, int)
+
+        # Check image values are in [-1, 1]
+        assert torch.all(x <= 1.0)
+        assert torch.all(x >= -1.0)
